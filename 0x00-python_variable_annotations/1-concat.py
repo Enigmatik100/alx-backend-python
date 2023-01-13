19b0eb9b04ef1204f1a0f9e83dedda5f2877ccfc
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-13 12:32:08 +0100

    1-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""concat two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return concatenation of str1 and str2
+    :param str1: str
+        first string
+    :param str2: str
+        second string
+    :return: string
+        concatenation of str1 and str2
+    """
+
+    return str1 + str2
