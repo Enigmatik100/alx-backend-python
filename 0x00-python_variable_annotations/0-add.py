0c1e3534ed69494b6ebbf282958f746c389bd809
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-13 12:24:39 +0100

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""add two float"""
+
 
 def add(a: float, b: float) -> float:
+    """
+
+    :param a: float
+        first number
+    :param b: float
+        second number
+    :return: float
+        sum of a and b
+    """
     return a + b
