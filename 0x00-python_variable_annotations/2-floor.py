68541153a7316d47a2f5e9adafc77790e8b5c424
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-13 12:36:51 +0100

    2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""get the floor of  float number"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns floor of n
+    :param n: float
+       the float number
+    :return: int
+        the floor of n
+    """
+
+    return math.floor(n)
