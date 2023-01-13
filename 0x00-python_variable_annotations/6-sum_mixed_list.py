1610c796892390c4f5d305afe9e92d02a556fc2d
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-13 13:02:32 +0100

    6-sum_mixed_list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-"""sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float"""
+"""
+sum_mixed_list which takes a list mxd_lst of integers
+and floats and returns their sum as a float
+"""
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
-    takes a list mxd_lst of integers and floats as argument and returns their sum as a float
+    takes a list mxd_lst of integers and floats as argument
+     and returns their sum as a float
     :param mxd_lst:
     :return: sum of mxd_lst elements as float
     """
