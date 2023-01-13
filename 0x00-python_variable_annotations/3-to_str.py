c0b85747c320401097cb0b41788e43ba375d1e25
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-13 13:32:10 +0100

    fix my code

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,8 +4,9 @@
 
 def to_str(n: float) -> str:
     """
-    takes a float n as argument and returns the string representation of the float.
-    param n: the float number
+    takes a float n as argument and returns the string representation
+    of the float.
+    :param n: the float number
     :return: string representation of the float
     """
 
